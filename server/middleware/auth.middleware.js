b393f5b6808c100417a10fe5771ba0fcecaf4813
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-08-15 16:09:07 +0500

    Eighth Commit

--- server/middleware/auth.middleware.js
+++ server/middleware/auth.middleware.js
@@ -2,15 +2,15 @@ const jwt = require("jsonwebtoken");
 const User = require("../models/User.model");
 
 const authUser = async (req, res, next) => {
+  const token = req.cookie.token;
+  if (!token) {
+    return res
+    .status(401)
+    .json({ error: "Access denied. No token provided." });
+  }
+  
   try {
-    const token = req.cookie.token;
-    if (!token) {
-      return res
-        .status(401)
-        .json({ error: "Access denied. No token provided." });
-    }
     const decode = jwt.verify(token, process.env.JWT_SECRET);
-
     const user = await User.findById(decode.id);
     if (!user) {
       return res.status(404).json({ error: "User not found." });
