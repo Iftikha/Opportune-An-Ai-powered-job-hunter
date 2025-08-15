e0a1b6b5770b0a23a51834501157d730918ae175
KhokharWelfare <khokharwelfare2024@gmail.com>
2025-08-15 15:46:01 +0500

    Seventh Commit

--- server/controllers/info.controller.js
+++ server/controllers/info.controller.js
@@ -3,23 +3,21 @@ const jwt = require('jsonwebtoken');
 
 const getUserInfo = async (req, res) => {
 
+    const lo
+ token = req.cookies.token; // cookie me se token le
+    if (!token) {
+        return res.status(401).json({ error: "Token missing" });
+    }
+
     try {
-        const token = req.cookies.token; // cookie se token
-        if (!token) {
-            return res.status(401).json({ error: "No token provided" });
-        }
-
         const decoded = jwt.verify(token, process.env.JWT_SECRET);
+        const userId = decoded.id; // yahi tumhara user ka _id hai
 
-        const user = await User.findById(decoded.id);
-        if (!user) {
-            return res.status(404).json({ error: "User not found" });
-        }
-
-        res.status(200).json({ message: "Success", user});  // request me user attach kar rahe hain
+        // ab tum userId se DB query kar sakte ho
+        res.json({ userId });
     } catch (err) {
         return res.status(401).json({ error: "Invalid token" });
-    }    
+    }
 }
 
 module.exports = getUserInfo;
